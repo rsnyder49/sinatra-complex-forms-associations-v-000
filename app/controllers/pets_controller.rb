34ad93d6bf58c85d3d77a00f2a995cca644a4bf9
Robert Snyder <r.snyder49@gmail.com>
2018-11-12 22:35:33 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,11 +2,13 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
+    
     erb :'/pets/index' 
   end
 
   get '/pets/new' do 
     @owners = Owner.all
+    
     erb :'/pets/new'
   end
 
@@ -21,7 +23,14 @@ class PetsController < ApplicationController
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
+    
     erb :'/pets/show'
+  end
+  
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    
+    erb :'/pets/edit'
   end
 
   patch '/pets/:id' do 
